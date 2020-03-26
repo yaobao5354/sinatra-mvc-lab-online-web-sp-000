b426b98cf5762f1a605cd2fb11ebc254e524ca28
yaoyu bao <yaobao5354@gmail.com>
2020-03-26 05:30:25 +0000

    Automatically backed up by Learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -5,8 +5,9 @@ def piglatinize(word)
   if %w(a e i o u).include?(word[0])
     piglatinized = word + 'way'
   else
-    binding.pry
+    first_vowel = word[/[aeiou]/]
     char = word.split(/[aeiou]/,2)
+    piglatinized = "#{char[1]}+#{char[0]}+ay"
   end
 end
 
