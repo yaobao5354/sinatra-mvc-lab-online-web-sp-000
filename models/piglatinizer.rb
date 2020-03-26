91a7dacde4a04e0bc7e4833063453fb7ee07b7d4
yaoyu bao <yaobao5354@gmail.com>
2020-03-26 05:40:40 +0000

    Automatically backed up by Learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -3,14 +3,27 @@ class PigLatinizer
 
 def piglatinize(sentence)
   words_array = sentence.split(" ")
-  binding.pry
-  if %w(a e i o u A E I O U).include?(word[0])
-    piglatinized = word + 'way'
+  if words_array.length == 1
+    if %w(a e i o u A E I O U).include?(word[0])
+      piglatinized = word + 'way'
+    else
+      first_vowel = word[/[aeiouAEIOU]/]
+      char = word.split(/[aeiouAEIOU]/,2)
+      piglatinized = first_vowel+char[1]+char[0]+'ay'
+    end
   else
-    first_vowel = word[/[aeiouAEIOU]/]
-    char = word.split(/[aeiouAEIOU]/,2)
-    piglatinized = first_vowel+char[1]+char[0]+'ay'
+    words_array.collect do |word|
+      if %w(a e i o u A E I O U).include?(word[0])
+        piglatinized = word + 'way'
+      else
+        first_vowel = word[/[aeiouAEIOU]/]
+        char = word.split(/[aeiouAEIOU]/,2)
+        piglatinized = first_vowel+char[1]+char[0]+'ay'
+      end
+    end
+    binding.pry
   end
+
 end
 
 end
